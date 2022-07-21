ad5820f8e47618d5dce3902f0395e6013f5952da
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-21 10:56:41 -0400

    populating characters using objects

--- south_park.js
+++ south_park.js
@@ -1,4 +1,4 @@
-//this will populate the text
+//images assigned to variables where variable name is character name
 let cat = "https://static.wikia.nocookie.net/southpark/images/5/5b/Pets-kennys-cat.png/revision/latest?cb=20200906152210"
 let lemmiwinks = "https://static.wikia.nocookie.net/southpark/images/b/b8/Lemmiwinks_%282%29.png/revision/latest/scale-to-width-down/700?cb=20161218172346";
 let mcnuggets = "https://static.wikia.nocookie.net/southpark/images/6/6b/Non-human-boys-pets-mcnugget.png/revision/latest?cb=20170604094830"
@@ -21,11 +21,8 @@ let mkay = "https://static.wikia.nocookie.net/southpark/images/5/58/Mr_Mackey_Ne
 let gopher = "https://static.wikia.nocookie.net/southpark/images/b/b0/Dramatic-look-gopher.png/revision/latest?cb=20170605100321"
 let chef = "https://static.wikia.nocookie.net/southpark/images/3/38/JeromeChef.png/revision/latest/scale-to-width-down/700?cb=20160402120214";
 
-//create image node
-let chefNode = document.createElement('img');
-//set src of image node to one of the characters' image link
-chefNode.src = chef;
-/*get characters div
+
+/*get characters div which will hold all our character images 
 1. get element by id => return one html element
 2. get elements by class => returns an array
 3. query selector => returns first matching element
@@ -33,15 +30,83 @@ class -> .characters
 id ->  #characters
 tag -> div
 */
-const charactersDiv = document.querySelector('.characters') //returns first matching element
+const charactersDiv = document.querySelector('.characters') 
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ADD NEW CHARACTER ON FORM SUBMIT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//1. get form node and save as a constant
+const form = document.querySelector("#form");
+//event: string (defines event), callback function (code to run when event is triggered)
+//2. event listener on form submit 
+form.addEventListener('submit', function(e) {
+    //prevent default
+    e.preventDefault();
+    //get the text user submitted 
+    let newValue = e.target.imageText.value;
+    //make image node 
+    let newNode = document.createElement('img');
+    //set src to user submitted text 
+    newNode.src = newValue 
+    //append image node to charactersDiv
+    charactersDiv.appendChild(newNode);
+});
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FETCH STATEMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+//initialize episodes variable with empty array...demonstrates fetch data is not accessible outside of fetch
+let episodes = [];
+//fetch statement returns a promise that takes its sweet time to evaluate while the rest of code in file runs
+//this is asynchronous code
+fetch('https://spapi.dev/api/episodes')
+.then(res => res.json()) //translate response into readable json
+.then(data => {
+    episodes = data.data;
+    //ONCE YOU'RE IN THE PROMISED LAND YOU CANNOT LEAVE
+    episodes.forEach((el) => {
+        
+        let pic = el.thumbnail_url
+        let title = el.name; 
+
+        //creating nodes
+        let container = document.createElement('div');
+        let titleNode = document.createElement('h3');
+        let epiNode = document.createElement('img') //node for episode thumbnail image
+
+        container.classList.add('episode-container')
+        //populating nodes
+        titleNode.innerText = title;
+        epiNode.src = pic;
+
+        //appending things
+       container.append(titleNode);
+       container.append(epiNode);
+       document.body.append(container); //container will now show up in browser
+    });
+})
+.catch(err => console.log(err)); //if anything fails print error
+
+//runs before promise gets fulfilled, episodes remains empty
+console.log('outside fetch statement episodes is', episodes);
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PUTTING CHARACTERS IN OBJECTS VS ARRAYS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /*
+Arrays - ordered, list, access things by index
+['dune', 'LoL', 'animorphs', 'brave new world', 'rick and morty']
 
+Objects - unordered, key/value pairs, access value with the key
+{
+    tsering: 'dune',
+    kaven: 'LoL',
+    carlos: 'animorphs',
+    george: 'brave new world',
+    brandon: 'rick and morty',
+}
+key does not need to be in quotes 
+values need to be stored as appropriate data type
 */
-//insert node into characters div
-//charactersDiv.append(chefNode);
 
-//I don't want to manually type these lines of code for each character's image
-//Put characters in a list instead and iterate through
+
+
+//put characters in list, you can only access by index number
 const characterList = [
     kyle,
     cartman,
@@ -66,95 +131,20 @@ const characterList = [
     lemmiwinks
 ]
 
-//iterate over charactersList with forEach
-//index parameter is optional
-//forEach is being passed a callback method that REQUIRES a parameter representing current element
-characterList.forEach((character, index) => {
+/*
+    objects
+    put characters in object and you can associate data
+    in our case we our associating the actual character name with their image link
 
-    //container div for numberNode and imageNode (so I can format better)
-    let containerNode = document.createElement('div');
+    nonHumans.mickey <- looks directly for key called 'mickey' and returns value
 
-    //number node using index
-    let numberNode = document.createElement('h2');
-    numberNode.innerText = index;
-
-    //create image node
-    let imageNode = document.createElement('img');
-    //populate image src 
-    imageNode.src = character
-    //update id with index -> DON'T. USE. NUMBERS. THIS IS ONLY FOR DEMONSTRATION. LETTERS. ONLY.
-    imageNode.id = index;
-    //example of changing width in javascript
-    imageNode.style.width = "128px";
-
-    //do all the appending
-    containerNode.append(numberNode);
-    containerNode.append(imageNode)
-    charactersDiv.appendChild(containerNode);
-})
-
-//add new character on form submit
-
-//1. get form node and save as a constant
-const form = document.querySelector("#form");
-//event: string (defines event), callback function (code to run when event is triggered)
-//2. event listener on form submit 
-form.addEventListener('submit', function(e) {
-    //prevent default
-    e.preventDefault();
-    //get the text user submitted 
-    let newValue = e.target.imageText.value;
-    //make image node 
-    let newNode = document.createElement('img');
-    //set src to user submitted text 
-    newNode.src = newValue 
-    //append image node to charactersDiv
-    charactersDiv.appendChild(newNode);
-});
-
-let episodes = [];
-//fetch statement returns a promise
-fetch('https://spapi.dev/api/episodes')
-.then(res => res.json()) //translate response into readable json
-.then(data => {
-    episodes = data.data;
-    console.log('in fetch statement', episodes);
-    episodes.forEach((el, i) => {
-        let container = document.createElement('div');
-        let pic = el.thumbnail_url
-        let title = el.name; 
-        let titleNode = document.createElement('h3');
-        titleNode.innerText = title;
-        console.log(pic);
-        let epiNode = document.createElement('img')
-        epiNode.src = pic;
-        charactersDiv.append(epiNode);
-        charactersDiv.append(titleNode);
-    });
-})
-.catch(err => console.log(err)); //if anything fails print error
-
-//runs before promise gets fulfilled
-console.log('outside fetch statement episodes is', episodes);
-
-
-
+    using brackets as keys will evaluate what is in the bracket
+    nonHumans[character] <- will evaluate character as variable
+    nonHumans['some-example'] <- use brackets and strings when key contains reserved symbols
     
-
-
-
-
-
-
-
-
-//fetch version
-//https://spapi.dev/api/episodes/1
-    
-
-/*objects*/
+*/
 const child = {
-    kyle,
+    kyle, //shorthand for kyle: kyle...I already declared kyle at top of file
     cartman,
     kenny,
     stan,
@@ -164,9 +154,10 @@ const child = {
     amanda,
     charlotte,
     ike
-}
+};
+
 const adults = {
-    mkay,
+    mkay, //shorthand for mkay: mkay...I already declared mkay at top of file 
     pc,
     minister,
     chef
@@ -182,3 +173,31 @@ const nonHumans = {
 }
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~USING OBJECTS TO POPULATE CHARACTERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//1. get keys as an array with Object.keys(nonHumans)
+//2. loop through keys array with forEach
+//3. access each value in object using current key with nonHumans[character]
+    
+Object.keys(nonHumans).forEach((character, index) => {
+
+    //container div for numberNode and imageNode (so I can format better)
+    let containerNode = document.createElement('div');
+
+    //character name node (stored in character)
+    let numberNode = document.createElement('h2');
+    numberNode.innerText = character;
+
+    //image node with current key's (the character variable) value
+    let imageNode = document.createElement('img');
+    imageNode.src = nonHumans[character] //populate image src, we need to evaluate character to current key
+
+    //update id with character's name (stored in character)
+    imageNode.id = character;
+    //example of changing width in javascript
+    imageNode.style.width = "128px";
+
+    //do all the appending
+    containerNode.append(numberNode);
+    containerNode.append(imageNode)
+    charactersDiv.appendChild(containerNode);
+})
