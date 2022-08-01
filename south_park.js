3aeb0408d8e2f6647804fd6739145021c2700187
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-01 09:01:40 -0400

    added json

--- south_park.js
+++ south_park.js
@@ -1,20 +1,3 @@
-//images assigned to variables where variable name is character name
-let lemmiwinks = "https://static.wikia.nocookie.net/southpark/images/b/b8/Lemmiwinks_%282%29.png/revision/latest/scale-to-width-down/700?cb=20161218172346";
-let mickey = "https://static.wikia.nocookie.net/southpark/images/d/d4/Mickey_Mouse.png/revision/latest?cb=20160912202212";
-let pc = "https://static.wikia.nocookie.net/southpark/images/4/44/PCprincipal1.png/revision/latest?cb=20211125042014";
-let ike = "https://static.wikia.nocookie.net/southpark/images/a/af/Ike-current.png/revision/latest/scale-to-width-down/656?cb=20180521124521";
-let kyle = "https://static.wikia.nocookie.net/southpark/images/9/95/Kyle-broflovski.png/revision/latest?cb=20190411033301"
-let cartman = "https://upload.wikimedia.org/wikipedia/en/7/77/EricCartman.png"
-let kenny = "https://static.wikia.nocookie.net/southpark/images/6/6f/KennyMcCormick.png/revision/latest?cb=20160409020502";
-let stan = "https://upload.wikimedia.org/wikipedia/en/a/a7/StanMarsh.png";
-let towlie = "https://static.wikia.nocookie.net/southpark/images/0/07/Towelie-0.png/revision/latest/scale-to-width-down/700?cb=20211125042745";
-let tolkein = "https://static.wikia.nocookie.net/southpark/images/d/d3/Token_Black2.png/revision/latest?cb=20211125042745";
-let butters = "https://static.wikia.nocookie.net/southpark/images/0/06/ButtersStotch.png/revision/latest?cb=20190411032405";
-let heidi = "https://static.wikia.nocookie.net/southpark/images/b/b8/HeidiTurnerHat.png/revision/latest/scale-to-width-down/699?cb=20211125042015";
-let mkay = "https://static.wikia.nocookie.net/southpark/images/5/58/Mr_Mackey_New.png/revision/latest/scale-to-width-down/451?cb=20211125042917"
-let chef = "https://static.wikia.nocookie.net/southpark/images/3/38/JeromeChef.png/revision/latest/scale-to-width-down/700?cb=20160402120214";
-
-
 /*get characters div which will hold all our character images 
 1. get element by id => return one html element
 2. get elements by class => returns an array
@@ -28,19 +11,20 @@ const charactersDiv = document.querySelector('.characters')
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ADD NEW CHARACTER ON FORM SUBMIT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //1. get form node and save as a constant
 const form = document.querySelector("#form");
-//event: string (defines event), callback function (code to run when event is triggered)
-//2. event listener on form submit 
+
 form.addEventListener('submit', function(e) {
-    //prevent default
-    e.preventDefault();
-    //get the text user submitted 
-    let newValue = e.target.imageText.value;
-    //make image node 
-    let newNode = document.createElement('img');
-    //set src to user submitted text 
-    newNode.src = newValue 
-    //append image node to charactersDiv
-    charactersDiv.appendChild(newNode);
+    e.preventDefault(); //prevent default
+    let newValue = e.target.imageText.value; //get user text
+    let newNode = document.createElement('img'); //make image node 
+    newNode.src = newValue  //set src 
+    charactersDiv.appendChild(newNode);//append image
+    /*
+    
+    
+    TODO: post south park character
+    
+    
+    */
 });
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FETCH STATEMENT~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -125,26 +109,16 @@ const characterList = [
     nonHumans['some-example'] <- use brackets and strings when key contains reserved symbols
     
 */
-const child = {
-    kyle, //shorthand for kyle: kyle...I already declared kyle at top of file
+const nonHumans = {
+    towlie,
+    pc,
+    chef,
     cartman,
     kenny,
     stan,
-    butters,
     heidi,
     tolkein,
-    ike
-};
-
-const adults = {
-    mkay, //shorthand for mkay: mkay...I already declared mkay at top of file 
-    pc,
-    chef
-}
-const nonHumans = {
-    towlie,
-    mickey,
-    lemmiwinks 
+    ike 
 }
 
 
